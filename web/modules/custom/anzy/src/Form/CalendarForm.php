84ba7b7f22284c8ef5b5eae674f0d58f0b40513b
Oleg <O.Tseiko@gmail.com>
2021-08-02 15:24:51 +0300

    finished validation

--- web/modules/custom/anzy/src/Form/CalendarForm.php
+++ web/modules/custom/anzy/src/Form/CalendarForm.php
@@ -62,6 +62,20 @@ class CalendarForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $def = $this->load();
+    if (empty($def)) {
+      $def[0]['jan'] = 0;
+      $def[0]['feb'] = 0;
+      $def[0]['mar'] = 0;
+      $def[0]['apr'] = 0;
+      $def[0]['may'] = 0;
+      $def[0]['jun'] = 0;
+      $def[0]['jul'] = 0;
+      $def[0]['aug'] = 0;
+      $def[0]['sep'] = 0;
+      $def[0]['oct'] = 0;
+      $def[0]['nov'] = 0;
+      $def[0]['dec'] = 0;
+    }
     $headers = [
       t('Year'),
       t('Jan'),
@@ -177,6 +191,46 @@ class CalendarForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    $monthsVal = [
+      'Jan' => $form_state->getValue('Jan'),
+      'Feb' => $form_state->getValue('Feb'),
+      'Mar' => $form_state->getValue('Mar'),
+      'Apr' => $form_state->getValue('Apr'),
+      'May' => $form_state->getValue('May'),
+      'Jun' => $form_state->getValue('Jun'),
+      'Jul' => $form_state->getValue('Jul'),
+      'Aug' => $form_state->getValue('Aug'),
+      'Sep' => $form_state->getValue('Sep'),
+      'Oct' => $form_state->getValue('Oct'),
+      'Nov' => $form_state->getValue('Nov'),
+      'Dec' => $form_state->getValue('Dec'),
+    ];
+    $count = 0;
+    $values = 0;
+    $theLastOne = 0;
+    $offset = 0;
+    $fieldsName = [];
+    foreach ($monthsVal as $month => $number) {
+      if ($number != "") {
+        $count++;
+        $values++;
+        if ($theLastOne != 0) {
+          $count--;
+          $theLastOne = 0;
+        }
+      }
+      elseif ($count != 0) {
+        $fieldsName[$offset] = $month;
+        $theLastOne++;
+        $offset++;
+      }
+    }
+    $fieldsName = array_slice($fieldsName, 0, $offset - $theLastOne, TRUE);
+    if ($count != $values) {
+      foreach ($fieldsName as $name) {
+        $form_state->setErrorByName($name, t('There is a space in report, you need to fix') . " " . $name);
+      }
+    }
     return $form;
   }
 
@@ -188,18 +242,18 @@ class CalendarForm extends FormBase {
     if (empty($this->load())) {
       $result = $connection->insert('anzy')
         ->fields([
-          'jan' => $form_state->getValue('Jan') != '' ? $form_state->getValue('Jan') : 0,
-          'feb' => $form_state->getValue('Feb') != '' ? $form_state->getValue('Feb') : 0,
-          'mar' => $form_state->getValue('Mar') != '' ? $form_state->getValue('Mar') : 0,
-          'apr' => $form_state->getValue('Apr') != '' ? $form_state->getValue('Apr') : 0,
-          'may' => $form_state->getValue('May') != '' ? $form_state->getValue('May') : 0,
-          'jun' => $form_state->getValue('Jun') != '' ? $form_state->getValue('Jun') : 0,
-          'jul' => $form_state->getValue('Jul') != '' ? $form_state->getValue('Jul') : 0,
-          'aug' => $form_state->getValue('Aug') != '' ? $form_state->getValue('Aug') : 0,
-          'sep' => $form_state->getValue('Sep') != '' ? $form_state->getValue('Sep') : 0,
-          'oct' => $form_state->getValue('Oct') != '' ? $form_state->getValue('Oct') : 0,
-          'nov' => $form_state->getValue('Nov') != '' ? $form_state->getValue('Nov') : 0,
-          'dec' => $form_state->getValue('Dec') != '' ? $form_state->getValue('Dec') : 0,
+          'jan' => $form_state->getValue('Jan') != '' ? $form_state->getValue('Jan') : NULL,
+          'feb' => $form_state->getValue('Feb') != '' ? $form_state->getValue('Feb') : NULL,
+          'mar' => $form_state->getValue('Mar') != '' ? $form_state->getValue('Mar') : NULL,
+          'apr' => $form_state->getValue('Apr') != '' ? $form_state->getValue('Apr') : NULL,
+          'may' => $form_state->getValue('May') != '' ? $form_state->getValue('May') : NULL,
+          'jun' => $form_state->getValue('Jun') != '' ? $form_state->getValue('Jun') : NULL,
+          'jul' => $form_state->getValue('Jul') != '' ? $form_state->getValue('Jul') : NULL,
+          'aug' => $form_state->getValue('Aug') != '' ? $form_state->getValue('Aug') : NULL,
+          'sep' => $form_state->getValue('Sep') != '' ? $form_state->getValue('Sep') : NULL,
+          'oct' => $form_state->getValue('Oct') != '' ? $form_state->getValue('Oct') : NULL,
+          'nov' => $form_state->getValue('Nov') != '' ? $form_state->getValue('Nov') : NULL,
+          'dec' => $form_state->getValue('Dec') != '' ? $form_state->getValue('Dec') : NULL,
           'reptable' => 0,
           'year' => 0,
         ])
@@ -209,18 +263,18 @@ class CalendarForm extends FormBase {
       $result = $connection->update('anzy')
         ->condition('id', 1)
         ->fields([
-          'jan' => $form_state->getValue('Jan') != '' ? $form_state->getValue('Jan') : 0,
-          'feb' => $form_state->getValue('Feb') != '' ? $form_state->getValue('Feb') : 0,
-          'mar' => $form_state->getValue('Mar') != '' ? $form_state->getValue('Mar') : 0,
-          'apr' => $form_state->getValue('Apr') != '' ? $form_state->getValue('Apr') : 0,
-          'may' => $form_state->getValue('May') != '' ? $form_state->getValue('May') : 0,
-          'jun' => $form_state->getValue('Jun') != '' ? $form_state->getValue('Jun') : 0,
-          'jul' => $form_state->getValue('Jul') != '' ? $form_state->getValue('Jul') : 0,
-          'aug' => $form_state->getValue('Aug') != '' ? $form_state->getValue('Aug') : 0,
-          'sep' => $form_state->getValue('Sep') != '' ? $form_state->getValue('Sep') : 0,
-          'oct' => $form_state->getValue('Oct') != '' ? $form_state->getValue('Oct') : 0,
-          'nov' => $form_state->getValue('Nov') != '' ? $form_state->getValue('Nov') : 0,
-          'dec' => $form_state->getValue('Dec') != '' ? $form_state->getValue('Dec') : 0,
+          'jan' => $form_state->getValue('Jan') != '' ? $form_state->getValue('Jan') : NULL,
+          'feb' => $form_state->getValue('Feb') != '' ? $form_state->getValue('Feb') : NULL,
+          'mar' => $form_state->getValue('Mar') != '' ? $form_state->getValue('Mar') : NULL,
+          'apr' => $form_state->getValue('Apr') != '' ? $form_state->getValue('Apr') : NULL,
+          'may' => $form_state->getValue('May') != '' ? $form_state->getValue('May') : NULL,
+          'jun' => $form_state->getValue('Jun') != '' ? $form_state->getValue('Jun') : NULL,
+          'jul' => $form_state->getValue('Jul') != '' ? $form_state->getValue('Jul') : NULL,
+          'aug' => $form_state->getValue('Aug') != '' ? $form_state->getValue('Aug') : NULL,
+          'sep' => $form_state->getValue('Sep') != '' ? $form_state->getValue('Sep') : NULL,
+          'oct' => $form_state->getValue('Oct') != '' ? $form_state->getValue('Oct') : NULL,
+          'nov' => $form_state->getValue('Nov') != '' ? $form_state->getValue('Nov') : NULL,
+          'dec' => $form_state->getValue('Dec') != '' ? $form_state->getValue('Dec') : NULL,
           'reptable' => 0,
           'year' => 0,
         ])
