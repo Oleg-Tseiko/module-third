a4664ce6bd6ccb3fd12211755097391bddb3915c
Oleg <O.Tseiko@gmail.com>
2021-08-06 16:51:32 +0300

    ajax fixed add year validation

--- web/modules/custom/anzy/src/Form/CalendarForm.php
+++ web/modules/custom/anzy/src/Form/CalendarForm.php
@@ -40,25 +40,62 @@ class CalendarForm extends FormBase {
   }
 
   /**
+   * Provides quartal report for fields.
+   */
+  public function quartalReport(array $monthsVal) {
+    for ($i = $this->rowsCount; $i > -1; $i--) {
+      $monthsVal["Qfirst$i"] = [
+        $monthsVal["Jan$i"] == NULL ? 0 : round($monthsVal["Jan$i"], 2),
+        $monthsVal["Feb$i"] == NULL ? 0 : round($monthsVal["Feb$i"], 2),
+        $monthsVal["Mar$i"] == NULL ? 0 : round($monthsVal["Mar$i"], 2),
+      ];
+      $monthsVal["Qfirst$i"] = $monthsVal["Qfirst$i"][0] == 0 && $monthsVal["Qfirst$i"][1] == 0 && $monthsVal["Qfirst$i"][2] == 0 ? "" : round((($monthsVal["Qfirst$i"][0] + $monthsVal["Qfirst$i"][1] + $monthsVal["Qfirst$i"][2]) + 1) / 3, 2);
+      $monthsVal["Qsecond$i"] = [
+        $monthsVal["Apr$i"] == NULL ? 0 : round($monthsVal["Apr$i"], 2),
+        $monthsVal["May$i"] == NULL ? 0 : round($monthsVal["May$i"], 2),
+        $monthsVal["Jun$i"] == NULL ? 0 : round($monthsVal["Jun$i"], 2),
+      ];
+      $monthsVal["Qsecond$i"] = $monthsVal["Qsecond$i"][0] == 0 && $monthsVal["Qsecond$i"][1] == 0 && $monthsVal["Qsecond$i"][2] == 0 ? "" : round((($monthsVal["Qsecond$i"][0] + $monthsVal["Qsecond$i"][1] + $monthsVal["Qsecond$i"][2]) + 1) / 3, 2);
+      $monthsVal["Qthird$i"] = [
+        $monthsVal["Jul$i"] == NULL ? 0 : round($monthsVal["Jul$i"], 2),
+        $monthsVal["Aug$i"] == NULL ? 0 : round($monthsVal["Aug$i"], 2),
+        $monthsVal["Sep$i"] == NULL ? 0 : round($monthsVal["Sep$i"], 2),
+      ];
+      $monthsVal["Qthird$i"] = $monthsVal["Qthird$i"][0] == 0 && $monthsVal["Qthird$i"][1] == 0 && $monthsVal["Qthird$i"][2] == 0 ? "" : round((($monthsVal["Qthird$i"][0] + $monthsVal["Qthird$i"][1] + $monthsVal["Qthird$i"][2]) + 1) / 3, 2);
+      $monthsVal["Qfourth$i"] = [
+        $monthsVal["Oct$i"] == NULL ? 0 : round($monthsVal["Oct$i"], 2),
+        $monthsVal["Nov$i"] == NULL ? 0 : round($monthsVal["Nov$i"], 2),
+        $monthsVal["Dec$i"] == NULL ? 0 : round($monthsVal["Dec$i"], 2),
+      ];
+      $monthsVal["Qfourth$i"] = $monthsVal["Qfourth$i"][0] == 0 && $monthsVal["Qfourth$i"][1] == 0 && $monthsVal["Qfourth$i"][2] == 0 ? "" : round((($monthsVal["Qfourth$i"][0] + $monthsVal["Qfourth$i"][1] + $monthsVal["Qfourth$i"][2]) + 1) / 3, 2);
+      $monthsVal["YTD$i"] = [
+        $monthsVal["Qfirst$i"] == "" ? 0 : round($monthsVal["Qfirst$i"], 2),
+        $monthsVal["Qsecond$i"] == "" ? 0 : round($monthsVal["Qsecond$i"], 2),
+        $monthsVal["Qthird$i"] == "" ? 0 : round($monthsVal["Qthird$i"], 2),
+        $monthsVal["Qfourth$i"] == "" ? 0 : round($monthsVal["Qfourth$i"], 2),
+      ];
+      $monthsVal["YTD$i"] = $monthsVal["Qfirst$i"] == 0 && $monthsVal["Qsecond$i"] == 0 && $monthsVal["Qthird$i"] == 0 && $monthsVal["Qfourth$i"] == 0 ? "" : round((($monthsVal["YTD$i"][0] + $monthsVal["YTD$i"][1] + $monthsVal["YTD$i"][2] + $monthsVal["YTD$i"][3]) + 1) / 4, 2);
+    }
+    return $monthsVal;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $inputes = $form_state->getUserInput();
-    $session = \Drupal::request()->getSession();
     $quartalsVal = [];
-    if (!empty($inputes) && $session->get('Rebuild') == 1) {
+    if (!empty($inputes) && $inputes["_triggering_element_value"] == "Add year") {
       $this->rowsCount = $inputes["hidden"];
       $this->rowsCount++;
-      $quartalsVal = $session->get('monthsVal');
     }
     elseif (!empty($inputes)) {
       $this->rowsCount = $inputes["hidden"];
     }
-    if ($session->get('falseValid') != 1) {
-      $quartalsVal = $session->get('monthsVal');
-    }
-    else {
-      $quartalsVal = [];
+    if (isset($inputes["_triggering_element_value"])) {
+      if ($inputes["_triggering_element_value"] == "Submit") {
+        $quartalsVal = $this->quartalReport($inputes);
+      }
     }
     $headers = [
       t('Year'),
@@ -203,6 +240,7 @@ class CalendarForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    $inputes = $form_state->getUserInput();
     $monthsVal = [];
     for ($i = $this->rowsCount; $i > -1; $i--) {
       $monthsVal["Jan$i"] = $form_state->getValue("Jan$i");
@@ -239,11 +277,10 @@ class CalendarForm extends FormBase {
       }
     }
     $fieldsName = array_slice($fieldsName, 0, $offset - $theLastOne, TRUE);
-    if ($count != $values) {
+    if ($count != $values && $inputes["_triggering_element_value"] == "Submit") {
       foreach ($fieldsName as $name) {
         $form_state->setErrorByName($name, t('There is a space in report, you need to fix'));
       }
-      \Drupal::request()->getSession()->set('falseValid', 1);
     }
     return $form;
   }
@@ -252,8 +289,6 @@ class CalendarForm extends FormBase {
    * Adding new year table to the report.
    */
   public function addYear(array &$form, FormStateInterface $form_state) {
-    $form_state->setRebuild(TRUE);
-    \Drupal::request()->getSession()->set('Rebuild', 1);
     return $form['wrapper'];
   }
 
@@ -261,53 +296,13 @@ class CalendarForm extends FormBase {
    * {@inheritDoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $session = \Drupal::request()->getSession();
-    $monthsVal = [];
-    for ($i = $this->rowsCount; $i > -1; $i--) {
-      $monthsVal["Qfirst$i"] = [
-        $form_state->getValue("Jan$i") == NULL ? 0 : round($form_state->getValue("Jan$i"), 2),
-        $form_state->getValue("Feb$i") == NULL ? 0 : round($form_state->getValue("Feb$i"), 2),
-        $form_state->getValue("Mar$i") == NULL ? 0 : round($form_state->getValue("Mar$i"), 2),
-      ];
-      $monthsVal["Qfirst$i"] = $monthsVal["Qfirst$i"][0] == 0 && $monthsVal["Qfirst$i"][1] == 0 && $monthsVal["Qfirst$i"][2] == 0 ? "" : round((($monthsVal["Qfirst$i"][0] + $monthsVal["Qfirst$i"][1] + $monthsVal["Qfirst$i"][2]) + 1) / 3, 2);
-      $monthsVal["Qsecond$i"] = [
-        $form_state->getValue("Apr$i") == NULL ? 0 : round($form_state->getValue("Apr$i"), 2),
-        $form_state->getValue("May$i") == NULL ? 0 : round($form_state->getValue("May$i"), 2),
-        $form_state->getValue("Jun$i") == NULL ? 0 : round($form_state->getValue("Jun$i"), 2),
-      ];
-      $monthsVal["Qsecond$i"] = $monthsVal["Qsecond$i"][0] == 0 && $monthsVal["Qsecond$i"][1] == 0 && $monthsVal["Qsecond$i"][2] == 0 ? "" : round((($monthsVal["Qsecond$i"][0] + $monthsVal["Qsecond$i"][1] + $monthsVal["Qsecond$i"][2]) + 1) / 3, 2);
-      $monthsVal["Qthird$i"] = [
-        $form_state->getValue("Jul$i") == NULL ? 0 : round($form_state->getValue("Jul$i"), 2),
-        $form_state->getValue("Aug$i") == NULL ? 0 : round($form_state->getValue("Aug$i"), 2),
-        $form_state->getValue("Sep$i") == NULL ? 0 : round($form_state->getValue("Sep$i"), 2),
-      ];
-      $monthsVal["Qthird$i"] = $monthsVal["Qthird$i"][0] == 0 && $monthsVal["Qthird$i"][1] == 0 && $monthsVal["Qthird$i"][2] == 0 ? "" : round((($monthsVal["Qthird$i"][0] + $monthsVal["Qthird$i"][1] + $monthsVal["Qthird$i"][2]) + 1) / 3, 2);
-      $monthsVal["Qfourth$i"] = [
-        $form_state->getValue("Oct$i") == NULL ? 0 : round($form_state->getValue("Oct$i"), 2),
-        $form_state->getValue("Nov$i") == NULL ? 0 : round($form_state->getValue("Nov$i"), 2),
-        $form_state->getValue("Dec$i") == NULL ? 0 : round($form_state->getValue("Dec$i"), 2),
-      ];
-      $monthsVal["Qfourth$i"] = $monthsVal["Qfourth$i"][0] == 0 && $monthsVal["Qfourth$i"][1] == 0 && $monthsVal["Qfourth$i"][2] == 0 ? "" : round((($monthsVal["Qfourth$i"][0] + $monthsVal["Qfourth$i"][1] + $monthsVal["Qfourth$i"][2]) + 1) / 3, 2);
-      $monthsVal["YTD$i"] = [
-        $monthsVal["Qfirst$i"] == "" ? 0 : round($monthsVal["Qfirst$i"], 2),
-        $monthsVal["Qsecond$i"] == "" ? 0 : round($monthsVal["Qsecond$i"], 2),
-        $monthsVal["Qthird$i"] == "" ? 0 : round($monthsVal["Qthird$i"], 2),
-        $monthsVal["Qfourth$i"] == "" ? 0 : round($monthsVal["Qfourth$i"], 2),
-      ];
-      $monthsVal["YTD$i"] = $monthsVal["Qfirst$i"] == 0 && $monthsVal["Qsecond$i"] == 0 && $monthsVal["Qthird$i"] == 0 && $monthsVal["Qfourth$i"] == 0 ? "" : round((($monthsVal["YTD$i"][0] + $monthsVal["YTD$i"][1] + $monthsVal["YTD$i"][2] + $monthsVal["YTD$i"][3]) + 1) / 4, 2);
-    }
-    $session->set('monthsVal', $monthsVal);
     \Drupal::messenger()->addMessage($this->t('Report updated successfully'), 'status', TRUE);
   }
 
   /**
-   * Adding new year table to the report.
+   * Triggers validation and quarterly report.
    */
   public function submitCall(array &$form, FormStateInterface $form_state) {
-    $form_state->setRebuild(TRUE);
-    $session = \Drupal::request()->getSession();
-    $session->set('Rebuild', 0);
-    $session->set('falseValid', 0);
     return $form['wrapper'];
   }
 
